9e24257bc5e53d778e4413fdd4503d6e88e27da8
大庭秀斗 <shuto.0028.0618@gmail.com>
2021-06-19 23:26:15 +0900

    add store

--- react_app/src/index.tsx
+++ react_app/src/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import createStore from './redux/store/store';
+
+const store = createStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 

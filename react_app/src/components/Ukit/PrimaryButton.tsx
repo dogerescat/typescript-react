bd243ceb5684f9f0f636ae301d1d4b9e168543f6
大庭秀斗 <shuto.0028.0618@gmail.com>
2021-06-24 21:52:48 +0900

    add material ui

--- react_app/src/components/Ukit/PrimaryButton.tsx
+++ react_app/src/components/Ukit/PrimaryButton.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/styles';
 
+interface Props {
+    label: string;
+    onClick: () => void;
+}
+
 const useStyles = makeStyles({
         "button": {
             backgroundColor: '#def7ff',
@@ -9,11 +14,11 @@ const useStyles = makeStyles({
             fontSize: 16,
             height: 48,
             marginButton: 16,
-            width: 256
+            width: 150
         }
 });
 
-const PrimaryButton = (props: any) => {
+const PrimaryButton = (props: Props) => {
     const classes = useStyles();
     return (
         <Button className={classes.button} variant="contained" onClick={() => props.onClick()}>
--- react_app/src/templates/Home.tsx
+++ react_app/src/templates/Home.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getUserId, getUserName } from '../redux/users/selectors';
-import { State } from '../types/redux/user';
+import { useDispatch } from 'react-redux';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { getUserId, getUserName } from '../redux/users/selectors';
+// import { State } from '../types/redux/user';
 import { signOut } from '../redux/users/operator';
-import { PrimaryButton } from '../components/Ukit';
+import { PrimaryButton, ContentInput } from '../components/Ukit';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const selector = useSelector((state: State) => state);
-  const userId = getUserId(selector);
-  const userName = getUserName(selector);
+  // const selector = useSelector((state: State) => state);
+  // const userId = getUserId(selector);
+  // const userName = getUserName(selector);
   return (
     <>
       <h2>Home</h2>
-      <p>{userId}</p>
-      <p>{userName}</p>
-      <PrimaryButton label={"sign out"} onClick={() => dispatch(signOut())} />
+      {/* <p>{userId}</p>
+      <p>{userName}</p> */}
+      <div className="content-input">
+        <ContentInput/>
+        <ContentInput/>
+        <ContentInput/>
+      </div>
+      <div className="signout-button">
+        <PrimaryButton label={"sign out"} onClick={() => dispatch(signOut())} />
+      </div>
     </>
   );
 };

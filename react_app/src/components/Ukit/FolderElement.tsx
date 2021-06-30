ef9ab0ce4d2525df97882033b5f7a16d874a2579
大庭秀斗 <shuto.0028.0618@gmail.com>
2021-06-30 21:56:40 +0900

    add modal

--- react_app/src/components/Ukit/FolderElement.tsx
+++ react_app/src/components/Ukit/FolderElement.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import { MemoModal } from '.';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -27,33 +27,39 @@ const useStyles = makeStyles((theme: Theme) =>
 
 interface Props {
   title: string;
+  content: string;
 }
 
 const FolderElement = (props: Props) => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+      setOpen(true);
+    };  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography
-          className={classes.title}
-          color='textSecondary'
-          gutterBottom
-        >
-          {props.title}
-        </Typography>
-        <Typography className={classes.pos} color='textSecondary'>
-          adjective
-        </Typography>
-        <Typography variant='body2' component='p'>
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size='small'>Learn More</Button>
-      </CardActions>
-    </Card>
+    <>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color='textSecondary'
+            gutterBottom
+          >
+            {props.title}
+          </Typography>
+          <Typography variant='body2' component='p'>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button onClick={() => { handleOpen() }} size='small'>OPEN</Button>
+        </CardActions>
+      </Card>
+      <MemoModal title={props.title} content={props.content} isOpen={open} handleClose={() => {handleClose()}}/>
+    </>
   );
 };
 
--- react_app/src/components/Ukit/index.ts
+++ react_app/src/components/Ukit/index.ts
@@ -1,6 +1,6 @@
 export { default as PrimaryButton } from './PrimaryButton';
 export { default as TextInput } from './TextInput';
-export { default as ContentInput } from './ContentInput';
 export { default as Dropdown } from './Dropdown';
 export { default as FolderElement } from './FolderElement';
 export { default as BottomNavi } from './BottomNavi';
+export { default as MemoModal} from './MemoModal';

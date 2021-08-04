1eff3c5d54a0a041b6bbd7bc433c81af2528fd52
大庭秀斗 <shuto.0028.0618@gmail.com>
2021-08-04 19:10:37 +0900

    add upload image

---
+++ react_app/src/components/Ukit/ImageArea.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Button from '@material-ui/core/Button';
+
+interface Props {
+  id: string;
+  path: string;
+  deleteImage: (id: string) => void;
+}
+
+const ImageArea = (props: Props) => {
+  return (
+    <>
+      <div className='preview-image'>
+        <img alt='' id={props.id} src={props.path} />
+        <Button onClick={() => props.deleteImage(props.id)}>
+            <DeleteIcon />
+        </Button>
+      </div>
+      <p className='preview-image-comment'>画像は複数添付できません</p>
+    </>
+  );
+};
+export default ImageArea;
